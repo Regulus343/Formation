8a671477926910f2a748093fbb5e91682b25b5ee
Cody Jassman <codyjassman@gmail.com>
2015-03-11 20:54:23 -0600

    minor adjustments

--- src/views/load_js.blade.php
+++ src/views/load_js.blade.php
@@ -1,13 +1,16 @@
 {{-- Formation JS: Load --}}
+
 <script type="text/javascript" src="{{ Site::js('formation', 'regulus/formation') }}"></script>
 
 {{-- Formation JS: Initialize --}}
-<script type="text/javascript">
-	$(document).ready(function(){
 
+<script type="text/javascript">
+
+	$(document).ready(function()
+	{
 		Formation.setErrorSettings($.parseJSON('{!! Form::getJsonErrorSettings() !!}'));
 
 		Formation.setErrors($.parseJSON('{!! Form::getJsonErrors() !!}'));
-
 	});
+
 </script>
